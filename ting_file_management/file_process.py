e71bfead13f57bc9b12ec1866907f702ec0f2f9e
bruh-git <brunac.cardososilva@gmail.com>
2022-12-06 11:21:34 -0300

    feat: req 03

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,21 @@
-def process(path_file, instance):
-    """Aqui irá sua implementação"""
+from ting_file_management.file_management import txt_importer
+from ting_file_management.queue import Queue
+
+
+def process(path_file: str, instance: Queue):
+    if path_file in str(instance._data):
+        return
+
+    lines = txt_importer(path_file)
+
+    data = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(lines),
+        "linhas_do_arquivo": lines,
+    }
+
+    instance.enqueue(data)
+    print(data)
 
 
 def remove(instance):
