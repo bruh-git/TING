0f44803992dc2ed9a137ff3a4d07ac45ea52ce7f
bruh-git <brunac.cardososilva@gmail.com>
2022-12-06 15:37:27 -0300

    feat: req 06 e 07

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,49 @@
-def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+from ting_file_management.queue import Queue
 
 
-def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+def exists_word(word: str, instance: Queue):
+    list = []
+    for i in range(len(instance)):
+        search = instance.search(i)
+        data = [
+            {"linha": index + 1}
+            for index, line in enumerate(search["linhas_do_arquivo"])
+            if word in line.lower()
+        ]
+    result = {
+            "palavra": word,
+            "arquivo": search["nome_do_arquivo"],
+            "ocorrencias": data,
+        }
+
+    if not len(result["ocorrencias"]) > 0:
+        return []
+
+    if result:
+        list.append(result)
+
+    return list
+
+
+def search_by_word(word: str, instance: Queue):
+    list = []
+    for i in range(len(instance)):
+        search = instance.search(i)
+        data = [
+            {"linha": index + 1, "conteudo": line}
+            for index, line in enumerate(search["linhas_do_arquivo"])
+            if word in line.lower()
+        ]
+    result = {
+            "palavra": word,
+            "arquivo": search["nome_do_arquivo"],
+            "ocorrencias": data,
+        }
+
+    if not len(result["ocorrencias"]) > 0:
+        return []
+
+    if result:
+        list.append(result)
+
+    return list
